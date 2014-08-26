93df03dff371e44a8b574ffcde6de83c3ec13919
Luke Gesior <luke.gesior@target.com>
2014-08-26 14:09:04 -0500

    figured out joi validation - beginning to implement

--- Survey.js
+++ Survey.js
@@ -1,5 +1,8 @@
+"use strict";
+
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var Joi = require('joi');
 
 var Survey = new Schema({
   name: String,
@@ -16,11 +19,28 @@ var Survey = new Schema({
   }, ]
 });
 
-Survey.index({
-  name: 1,
-  owner: 1
-}, {
-  unique: true
-});
+// Survey.index({
+//   name: 1,
+//   owner: 1
+// }, {
+//   unique: true
+// });
+
+Survey.methods.joiValidate = function(obj) {
+
+  var schema = Joi.object().keys({
+		name: Joi.string().min(3).max(30).required(),
+		owner: Joi.string().min(3).max(30).required(),
+		maxResponses: Joi.number().integer().min(100).max(1000).required(),
+		campaign: Joi.string().required(),
+		costCenterId: Joi.string().required(),
+		netWorth: Joi.number().required(),
+    logoUrl: Joi.string(),
+    status: Joi.string(),
+    questions: Joi.array()
+	});
+	return Joi.validate(obj, schema, {allowUnknown:true});
+};
+
 
 module.exports = mongoose.model('Survey', Survey);
